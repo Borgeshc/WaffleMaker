5dc7cc3b06558303edcfd0f706e1fba78967713d
Borgeshc <hborges@digitaliris.com>
2016-12-06 18:43:47 -0600

    Fixed sams broken code

---
+++ Assets/Music.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip[] songs;
+    AudioSource source;
+    
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        if(!source.isPlaying)
+        {
+            source.clip = songs[Random.Range(0, songs.Length)];
+            source.Play();
+        }
+	}
+}
